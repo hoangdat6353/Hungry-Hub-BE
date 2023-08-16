fec6c5c85d3fef42023f60f77f6a6f51342266c1
Neon Nguyen <neon.n@itcgroup.io>
2023-08-16 16:41:53 +0700

    Update deploy database config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,13 +2,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'foodmanagement',
+  url: 'postgres://root:gyovS8eeXXHeHW4c3dzpJqTcCUCFtsY8@dpg-cje87s8cfp5c73bq3g8g-a.singapore-postgres.render.com/foodmanagement',
+  // host: 'dpg-cje87s8cfp5c73bq3g8g-a.singapore-postgres.render.com',
+  // port: 5432,
+  // username: 'root',
+  // password: 'gyovS8eeXXHeHW4c3dzpJqTcCUCFtsY8',
+  // database: 'foodmanagement',
   entities: [__dirname + '/../libs/entities/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
   migrations: [__dirname + '/../libs/migrations/**/*.ts'],
   subscribers: [__dirname + '/../libs/subcribers/**/*.ts'],
+  ssl: true, // Enable SSL
 };
