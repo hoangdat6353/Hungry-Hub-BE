c4b79491e526c471c70a607f74c81dbae0043418
Neon Nguyen <neon.n@itcgroup.io>
2023-08-28 20:30:00 +0700

    Init backend build 28/08

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,15 +2,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'foodmanagement',
+  // host: 'localhost',
+  // port: 5432,
+  // username: 'root',
+  // password: 'root',
+  //database: 'foodmanagement',
   entities: [__dirname + '/../libs/entities/*.entity{.ts,.js}'],
   synchronize: true,
   migrations: [__dirname + '/../libs/migrations/**/*.ts'],
   subscribers: [__dirname + '/../libs/subcribers/**/*.ts'],
-  // url: 'postgres://root:lbcVRiVlEDn04dS2IrujppfZzCKJOsh9@dpg-cjea5irbq8nc73cmvf40-a.oregon-postgres.render.com/hungryhub',
-  // ssl: true, // Enable SSL
+  url: 'postgres://root:lbcVRiVlEDn04dS2IrujppfZzCKJOsh9@dpg-cjea5irbq8nc73cmvf40-a.oregon-postgres.render.com/hungryhub',
+  ssl: true, // Enable SSL
 };
